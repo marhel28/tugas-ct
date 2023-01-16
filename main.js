d2b602b783a43616d5632d3e8bc28b30177d2463
marhel28 <67575383+marhel28@users.noreply.github.com>
2023-01-16 12:24:32 +0700

    add file

---
+++ main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.small-images', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
